794a9029abc21d5be4d89082e3416b92f1fe4afa
Luca Ricci <luca.ricci14@studio.unibo.it>
2013-09-11 18:05:03 +0200

    level_maze builder enhanced

--- game/.../core/challenges/level_maze/build.sh
+++ game/.../core/challenges/level_maze/build.sh
@@ -21,6 +21,7 @@ function maze_end {
 
 ln -f $level_path/begin_instructions.txt $playing_path/$1/$readme.txt
 
+# Create the dead_ends
 dead_end $f/$f/$f/$r/$f/$l
 dead_end $f/$f/$f/$r/$l/$r
 dead_end $f/$f/$f/$r/$l/$l
@@ -36,6 +37,16 @@ dead_end $f/$r/$l/$r/$f/$l
 dead_end $f/$r/$l/$r/$r
 dead_end $f/$r/$l/$l
 
+# Delete dead_end messages in not dead_end locations
+for i in $(find $playing_path -type f)
+  do
+    if [ ! -z $mayalinuxDebug ] ; then echo -e "file: $i:$(dirname $i):$playing_path" >&2 ; fi
+    test "$(dirname $i)" != "$playing_path" &&
+      test "$(ls -l `dirname $i` | grep -e "^d" | wc -l)" -gt "0" &&
+        rm $i
+done
+
+# Create the exit
 maze_end $f/$r/$l/$r/$f/$f/$f/$r/$l
 
 if [ ! -z $mayalinuxDebug ] ; then echo -e "level_maze built" >&2 ; fi
